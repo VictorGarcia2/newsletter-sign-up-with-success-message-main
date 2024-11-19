fbbb39219c0a26df29d05015739496854e46f55d
VictorGarcia2 <victrgarciaphoto@gmail.com>
2024-11-19 08:07:29 -0600

    finish task

---
+++ app.js
@@ -0,0 +1,32 @@
+const submit = document.querySelector('#submit')
+const dismiss = document.querySelector('#dismiss')
+const fin = document.querySelector('#finish')
+const card = document.querySelector('#card')
+const miInput = document.querySelector('#input')
+const label = document.querySelector('label')
+
+
+ 
+
+ 
+  function modalDismiss(){
+finish.style.display = "none"
+ card.style = "display: flex"
+ miInput.value = ""
+ miInput.style = ""
+}  
+submit.addEventListener('click', ()=>{
+    let rex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
+    let exp = rex.test(miInput.value)
+    console.log(exp);
+      if(exp){ 
+        miInput.style = "background-color: #57ff76; border: 1px solid #57ff76;"
+        finish.style.display = "block"
+     card.style.display = "none" 
+    } else{
+        miInput.style = "background-color: #ff62573e; border: 1px solid #ff6257;"
+        label.textContent = "Valid email required"
+        label.style = "color:#ff6257;"
+      }
+})
+dismiss.addEventListener('click', modalDismiss) 
