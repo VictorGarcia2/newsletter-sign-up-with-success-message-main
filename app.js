2feac411cca144682c19d9165f601fe7adde72cb
VictorGarcia2 <victrgarciaphoto@gmail.com>
2024-11-19 08:22:38 -0600

    email succes

--- app.js
+++ app.js
@@ -6,27 +6,31 @@ const miInput = document.querySelector('#input')
 const label = document.querySelector('label')
 
 
- 
 
- 
-  function modalDismiss(){
-finish.style.display = "none"
- card.style = "display: flex"
- miInput.value = ""
- miInput.style = ""
-}  
-submit.addEventListener('click', ()=>{
-    let rex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
-    let exp = rex.test(miInput.value)
-    console.log(exp);
-      if(exp){ 
-        miInput.style = "background-color: #57ff76; border: 1px solid #57ff76;"
-        finish.style.display = "block"
-     card.style.display = "none" 
-    } else{
-        miInput.style = "background-color: #ff62573e; border: 1px solid #ff6257;"
-        label.textContent = "Valid email required"
-        label.style = "color:#ff6257;"
-      }
+
+
+function modalDismiss() {
+  finish.style.display = "none"
+  card.style = "display: flex"
+  miInput.value = ""
+  miInput.style = ""
+}
+submit.addEventListener('click', () => {
+  let rex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+  let exp = rex.test(miInput.value)
+  console.log(exp);
+  if (exp) {
+    miInput.style = "background-color: #57ff76; border: 1px solid #57ff76;"
+    finish.style.display = "block"
+    card.style.display = "none"
+    document.querySelector('#finish').innerHTML = `<p>
+          A confirmation email has been sent to ${exp}
+          Please open it and click the button inside to confirm your subscription.
+        </p>`
+  } else {
+    miInput.style = "background-color: #ff62573e; border: 1px solid #ff6257;"
+    label.textContent = "Valid email required"
+    label.style = "color:#ff6257;"
+  }
 })
 dismiss.addEventListener('click', modalDismiss) 
